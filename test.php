a8ac707596d4007bd3f933a0affecd6c8925281d
ChangerHe <244737162@qq.com>
2017-08-12 23:51:58 +0800

    for test

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php 
+    $a = "张先生";
+	$tip = $a."Welcome to imooc";
+	
+    $b = "东边日出西边雨";	
+    $b.=",Sayed it's rain,but it's sunny"
+    
+	$c = "东边日出西边雨";	
+    $c = $c.",Sayed it's rain,but it's sunny"
+    
+	echo  $tip."<br />";
+	echo  $b."<br />";
+	echo  $c."<br />";
+?>
